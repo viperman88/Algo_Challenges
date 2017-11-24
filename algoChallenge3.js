1972daeed5f25c1949c149e999160d77ac3f46dd
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-24 18:28:38 -0500

    updated algos

--- algoChallenge3.js
+++ algoChallenge3.js
@@ -195,3 +195,20 @@ const alwaysHungry = (arr) => {
 };
 alwaysHungry([23, 'food', 'hello', false, 'food'])
 alwaysHungry([23, 'foo', 'hello', true])
+
+//Given array, swap first and last, third and third-tolast,
+//etc. Input[true,42,"Ada",2,"pizza"] becomes ["pizza",42,"Ada",2,true].
+//Change [1,2,3,4,5,6] to [6,2,4,3,5,1].
+const swapTowardsCenter = (arr) => {
+    let temp = arr[0];
+    let temp2 = arr[2];
+
+    arr.splice(0, 1, arr[arr.length - 1]);
+    arr.splice(arr[arr.length - 2], 1, temp);
+
+    arr.splice(2, 1, arr[arr.length - 3]);
+    arr.splice(3, 1, temp2);
+
+    console.log(arr);
+};
+swapTowardsCenter([1, 2, 3, 4, 5, 6]);
