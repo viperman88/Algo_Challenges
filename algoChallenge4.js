18b6792df71c3f36f204937dcdcb18dcba942467
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-26 18:20:41 -0500

    updated algos

--- algoChallenge4.js
+++ algoChallenge4.js
@@ -6,4 +6,15 @@ const scaleTheArray = (arr, num) => {
     }
     return arr;
 };
-console.log(scaleTheArray([2, 4, 6, 8, 10, 12, 14, 16], 10))
+console.log(scaleTheArray([2, 4, 6, 8, 10, 12, 14, 16], 10));
+
+//Stan learned something today: that reducing an array’s .length
+//immediately shortens it by that amount. Given array arr and number
+//X, remove all except the last X elements, and return arr (changed
+//and shorter). Given ([2,4,6,8,10],3), change the given array to
+//[6,8,10] and return it.
+const onlyKeepLastFew = (arr, x) => {
+    let newArr = arr.slice(x, arr.length);
+    return newArr;
+}
+console.log(onlyKeepLastFew([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
