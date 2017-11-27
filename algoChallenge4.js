7341705c572be52241854a02a6e88a5539fa7b34
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-27 12:33:40 -0500

    updated algos

--- algoChallenge4.js
+++ algoChallenge4.js
@@ -48,3 +48,22 @@ const whatHappensToday = () => {
 };
 
 whatHappensToday();
+
+//Your time with us will definitely make you smarter!
+//Let’s say a new Dojo student, Bogdan, entered with a
+//modest IQ of 101. How smart would Bogdan be at the
+//end of the bootcamp, if his IQ rose by .01 on the
+//first day, then went up by an additional .02 on the
+//second day, up by .03 more on the third day, etc….
+//all the way until increasing by .98 on his 98th day
+//(the end of 14 full weeks)?
+const soaringIQ = (iq) => {
+    let iqRise = 0.01;
+    for (let i = 1; i <= 98; i++) {
+        iq += iqRise;
+        iqRise += 0.01;
+        //console.log(iq);
+    }
+    return iq;
+};
+console.log(soaringIQ(101));
