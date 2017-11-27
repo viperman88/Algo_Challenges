38df5f169d13fe0b5df159f5343e606e52544218
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-27 12:49:23 -0500

    updated algos

--- algoChallenge4.js
+++ algoChallenge4.js
@@ -67,3 +67,29 @@ const soaringIQ = (iq) => {
     return iq;
 };
 console.log(soaringIQ(101));
+
+//Mr. Cerise teaches high school math. Write a function
+//that assigns and prints a letter grade, given an integer
+//representing a score from 0 to 100? Those getting 90+
+//get an ‘A’, 80-89 earn ‘B’, 70-79 is a ‘C’, 60-69 should
+//get a ‘D’, and lower than 60 receive ‘F’.
+//Example: given 88, you should log "Score:88. Grade: B".
+//Given the score 61, log the string "Score: 61. Grade: D".
+const letterGrade = (grade) => {
+    if (grade < 60) {
+        console.log(`Score: ${grade}, Grade: F`);
+    } else if (grade < 70) {
+        console.log(`Score: ${grade}, Grade: D`);
+    } else if (grade < 80) {
+        console.log(`Score: ${grade}, Grade: C`);
+    } else if (grade < 90) {
+        console.log(`Score: ${grade}, Grade: B`);
+    } else {
+        console.log(`Score: ${grade}, Grade: A`);
+    }
+};
+letterGrade(58);
+letterGrade(63);
+letterGrade(72);
+letterGrade(87);
+letterGrade(98);
