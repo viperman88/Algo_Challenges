891bffaec0b8b126e3d8c5f85859414b45f96ffb
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-12-04 16:10:33 -0500

    updated algos

--- fundAlgos.js
+++ fundAlgos.js
@@ -326,3 +326,30 @@ const clockHandAngles = seconds => {
 clockHandAngles(119730)
 clockHandAngles(3606)
 clockHandAngles(267891)
+
+// Make a function that takes a number of quarters (1 quarter = 1 game).
+// There is a 1 in 100 chance to win the slot machine (which will give you between
+//50 - 100 quarters -- use Math.random and Math.floor to pick a random number of coins).
+// While the user still has quarters, use Math.random to determine if they won.
+// If they ever win, return the number of quarters (ex: they had 50 remaining quarters
+//and won 90, so it should return 140). Return 0 if all the quarters were wasted.
+// Bonus (Only If You Have Time): Let the user pass the number they are willing to leave
+// (ex: If they won the lottery and still have 40 coins, they will keep playing until they
+//have collected 200 coins)
+const slots = (quarters, min, max) => {
+    let bet = 5;
+    while (quarters > min) {
+        let machineNum = Math.floor(Math.random() * 50);
+        let yourNum = Math.floor(Math.random() * 50);
+        if (machineNum === yourNum) {
+            let winnings = Math.floor(Math.random() * 50) + 51;
+            quarters += winnings;
+            console.log(`Congrats! You won ${winnings} quarters! You now have ${quarters}!`)
+        } else {
+            quarters -= bet;
+            console.log(`You lost ${bet} quarters. Your number was ${yourNum}, the machines was ${machineNum}. You now have ${quarters} quarters.`)
+        }
+    }
+    console.log(`You available quarters is ${quarters}. You reached you min/max set playing values.`)
+}
+slots(100, 20, 300);
