88aef1455e891f99cd1643f1201a896602e72a04
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-12-11 06:11:07 -0500

    updated algos

--- fundAlgos2.js
+++ fundAlgos2.js
@@ -55,3 +55,20 @@ const clockHandAngles = seconds => {
 clockHandAngles(119730)
 clockHandAngles(3606)
 clockHandAngles(267891)
+
+//Create the extractDigit(num,digitNum) function that given a number and
+//a digit number, returns the numeral value of that digit. 0 represents
+//the ones digit, 1 represents the tens digit, etc. Given (1824,2), return
+//8. Given (1824,0), return 4. Given (1824,7), return 0.
+
+//Second: handle negative digitNum values, where -1 represents tenths digit
+//(0.x), -2 represents hundredths digit (0.0x), etc. Given (123.45,-1), return 4.
+
+//Third: handle negative num values as well, doing what you think is appropriate.
+
+const extractDigit = (num, digitNum) => {
+    num += '';
+    num = num.split('').reverse().join('');
+    return num[digitNum];
+}
+console.log(extractDigit(1824, 2))
